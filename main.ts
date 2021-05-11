f1391cf25e76353cf098e088f9209f2702766be7
M4ldu <84003143+M4ldu@users.noreply.github.com>
2021-05-11 07:57:44 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,12 @@
+let Start = 0
+let elapsed = 0
+input.onButtonPressed(Button.A, function () {
+    Start = input.runningTime()
+})
+input.onButtonPressed(Button.B, function () {
+    elapsed = input.runningTime() - Start
+    basic.showNumber(Math.idiv(elapsed, 1000))
+})
+basic.forever(function () {
+	
+})
